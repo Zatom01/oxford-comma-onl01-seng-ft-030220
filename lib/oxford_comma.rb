d4421c65c39f83e431d154ae5288511bfbb032b2
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-04 07:48:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,14 @@
 def oxford_comma(array)
-  last_item=array[-1]
-  array.pop
-  array.push("and "+last_item)
+  
   
   if array.size >2
+    last_item=array[-1]
+    array.pop
+    array.push("and "+last_item)
     return array * ", "
   else
-    return array
-  
+    return array * " and "
+  end
   
     
 
