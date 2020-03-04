50519dd571554658d8b8af5e3f3b56b896b91908
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-04 07:49:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  
+  
+  if array.size >2
+    last_item=array[-1]
+    array.pop
+    array.push("and "+last_item)
+    return array * ", "
+  else
+    return array * " and "
+  end
+  
+    
 
 end
